ed94867fc1fb6128589e01427558c7b1dd28663b
David Buezas <david.buezas@gmail.com>
2022-04-01 19:48:21 +0200

    initial commit

---
+++ cc1101.h
@@ -0,0 +1,80 @@
+// https://github.com/dbuezas/esphome-cc1101
+
+#ifndef CC1101TRANSCIVER_H
+#define CC1101TRANSCIVER_H
+
+#include <ELECHOUSE_CC1101_SRC_DRV.h>
+
+int CC1101_module_count = 0;
+#define get_cc1101(id) (*((CC1101 *)id))
+
+class CC1101 : public PollingComponent, public Sensor {
+  int _SCK;
+  int _MISO;
+  int _MOSI;
+  int _CSN;
+  int _GDO0;
+  float _bandwidth;
+  
+  float _moduleNumber;
+  int _last_rssi = 0;
+
+  void setup() {
+    pinMode(_GDO0, INPUT);
+    ELECHOUSE_cc1101.addSpiPin(_SCK, _MISO, _MOSI, _CSN, _moduleNumber);
+    ELECHOUSE_cc1101.setModul(_moduleNumber);
+    ELECHOUSE_cc1101.Init();
+    ELECHOUSE_cc1101.setRxBW(_bandwidth);
+    ELECHOUSE_cc1101.setMHZ(_freq);
+    ELECHOUSE_cc1101.SetRx();
+  }
+
+ public:
+  float _freq;
+  CC1101(int SCK, int MISO, int MOSI, int CSN, int GDO0, float bandwidth,
+         float freq)
+      : PollingComponent(100) {
+    _SCK = SCK;
+    _MISO = MISO;
+    _MOSI = MOSI;
+    _CSN = CSN;
+    _GDO0 = GDO0;
+    _bandwidth = bandwidth;
+    _freq = freq;
+    _moduleNumber = CC1101_module_count++;
+  }
+
+  void beginTransmission() {
+    ELECHOUSE_cc1101.setModul(_moduleNumber);
+    ELECHOUSE_cc1101.SetTx();
+    pinMode(_GDO0, OUTPUT);
+    noInterrupts();
+  }
+  void endTransmission() {
+    interrupts();
+    pinMode(_GDO0, INPUT);
+    ELECHOUSE_cc1101.setModul(_moduleNumber);
+    ELECHOUSE_cc1101.SetRx();
+    ELECHOUSE_cc1101.SetRx();  // yes, twice
+  }
+  void setBW(float bandwidth) {
+    ELECHOUSE_cc1101.setRxBW(bandwidth);
+  }
+  void setFreq(float freq) {
+    ELECHOUSE_cc1101.setMHZ(freq);
+  }
+  bool rssi_on;
+  void update() override {
+    int rssi = 0;
+    if (rssi_on) {
+      ELECHOUSE_cc1101.setModul(_moduleNumber);
+      rssi = ELECHOUSE_cc1101.getRssi();
+    }
+    if (rssi != _last_rssi) {
+      publish_state(rssi);
+      _last_rssi = rssi;
+    }
+  }
+};
+
+#endif
---
+++ readme.md
@@ -0,0 +1,15 @@
+![image](https://esphome.io/_images/made-for-esphome-black-on-white.svg)
+# CC1101 support for ESPHome
+
+IMPORTANT: make sure to copy both `cc1101.yaml` and `cc1101.h`
+![image](./cc1101-variants.png)
+
+# Basic
+see cc1101.yaml
+
+# Advanced
+See cc1101-advanced.yaml
+Adds RSSI sensor and number components to tweak frequency and bandwidth on the fly.
+I use this to tweak the values to maximize reception.
+
+
