b34603edbc4e6745add895a75e9cf705d1bf7833
David Buezas <david.buezas@gmail.com>
2024-06-09 00:05:01 +0200

    Unify esp32 and 8266, both only use GDO0

--- cc1101.h
+++ cc1101.h
@@ -5,30 +5,24 @@
 
 #include <ELECHOUSE_CC1101_SRC_DRV.h>
 
+#include "esphome/components/remote_transmitter/remote_transmitter.h"
+
 int CC1101_module_count = 0;
-#define get_cc1101(id) (*((CC1101 *)id))
+#define get_cc1101(id) (*((CC1101*)id))
 
 class CC1101 : public PollingComponent, public Sensor {
   int _SCK;
   int _MISO;
   int _MOSI;
   int _CSN;
-  int _GDO0; // TX (and also RX if ESP8266)
-#ifdef USE_ESP32
-  int _GDO2; // RX
-#endif
+  int _GDO0;  // TX and also RX
   float _bandwidth;
-  
+  esphome::remote_transmitter::RemoteTransmitterComponent* _remote_transmitter;
   float _moduleNumber;
   int _last_rssi = 0;
 
   void setup() {
-#ifdef USE_ESP32
-    pinMode(_GDO0, OUTPUT);
-    pinMode(_GDO2, INPUT);
-#else
     pinMode(_GDO0, INPUT);
-#endif
     ELECHOUSE_cc1101.addSpiPin(_SCK, _MISO, _MOSI, _CSN, _moduleNumber);
     ELECHOUSE_cc1101.setModul(_moduleNumber);
     ELECHOUSE_cc1101.Init();
@@ -39,37 +33,36 @@ class CC1101 : public PollingComponent, public Sensor {
 
  public:
   float _freq;
-  CC1101(int SCK, int MISO, int MOSI, int CSN, int GDO0, 
-#ifdef USE_ESP32
-         int GDO2, 
-#endif
-         float bandwidth, float freq)
+  CC1101(int SCK, int MISO, int MOSI, int CSN, int GDO0, float bandwidth, float freq,
+         esphome::remote_transmitter::RemoteTransmitterComponent* remote_transmitter)
       : PollingComponent(100) {
     _SCK = SCK;
     _MISO = MISO;
     _MOSI = MOSI;
     _CSN = CSN;
     _GDO0 = GDO0;
-#ifdef USE_ESP32
-    _GDO2 = GDO2;
-#endif
     _bandwidth = bandwidth;
     _freq = freq;
     _moduleNumber = CC1101_module_count++;
+    _remote_transmitter = remote_transmitter;
   }
 
   void beginTransmission() {
     ELECHOUSE_cc1101.setModul(_moduleNumber);
     ELECHOUSE_cc1101.SetTx();
-#ifndef USE_ESP32
     pinMode(_GDO0, OUTPUT);
+#ifdef USE_ESP32
+    _remote_transmitter->setup();
+#endif
+#ifdef USE_ESP8266
     noInterrupts();
 #endif
   }
   void endTransmission() {
-#ifndef USE_ESP32
-    interrupts();
+    digitalWrite(_GDO0, 0);
     pinMode(_GDO0, INPUT);
+#ifdef USE_ESP8266
+    interrupts();
 #endif
     ELECHOUSE_cc1101.setModul(_moduleNumber);
     ELECHOUSE_cc1101.SetRx();
@@ -83,16 +76,16 @@ class CC1101 : public PollingComponent, public Sensor {
     ELECHOUSE_cc1101.setModul(_moduleNumber);
     ELECHOUSE_cc1101.setMHZ(freq);
   }
-  bool rssi_on;
+  bool rssi_on = false;
   void update() override {
     int rssi = 0;
     if (rssi_on) {
       ELECHOUSE_cc1101.setModul(_moduleNumber);
       rssi = ELECHOUSE_cc1101.getRssi();
-    }
-    if (rssi != _last_rssi) {
-      publish_state(rssi);
-      _last_rssi = rssi;
+      if (rssi != _last_rssi) {
+        publish_state(rssi);
+        _last_rssi = rssi;
+      }
     }
   }
 };
